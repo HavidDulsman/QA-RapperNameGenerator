95218561937d9ef12913cbf17947651d79dbbcd3
HavidDulsman <david.hulsman@academytrainee.com>
2020-03-27 20:38:43 +0000

    SFIA 2 week 1 finish

--- service_1/application/routes.py
+++ service_1/application/routes.py
@@ -1,11 +1,20 @@
 from flask import Flask, request, render_template, url_for, redirect
+from flask_mysqldb import MySQL
 from application import app
 import requests
 import random
 
+#MySQl Config
+app.config['MYSQL_HOST'] = '35.246.123.192'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Duddybear12'
+app.config['MYSQL_DB'] = 'rappernamegen'
+
 @app.route('/',methods=['GET'])
 def home():
-        response = requests.get('http://localhost:5003/generatedname')
+        response = requests.get('http://localhost:5003/generated3')
         print("hello!")
         generatedname = response.text
+        print(generatedname)
         return render_template('home.html',generatedname = generatedname, title = 'Home')
+
