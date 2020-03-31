4775b214ae97c0a336a3d93ee61298246aa26c3b
HavidDulsman <david.hulsman@academytrainee.com>
2020-03-31 08:44:12 +0000

    fixed sensitive data on SQL

--- service_1/application/__init__.py
+++ service_1/application/__init__.py
@@ -1,14 +1,15 @@
 from flask import Flask, request
 from flask_mysqldb import MySQL
 import requests
+import os
 
 app = Flask(__name__)
 
 #MySQl Config
-app.config['MYSQL_HOST'] = '35.246.123.192'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Duddybear12'
-app.config['MYSQL_DB'] = 'rappernamegen'
+app.config['MYSQL_HOST'] = os.environ.get('MYSQLHOST')
+app.config['MYSQL_USER'] = os.environ.get('MYSQLUSER')
+app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQLPASSWORD')
+app.config['MYSQL_DB'] = os.environ.get('MYSQLDB')
 
 from application import routes
 
