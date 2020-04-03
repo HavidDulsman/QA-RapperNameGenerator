f646aacb070283e1c44e48e2524e61a1c3205cf8
HavidDulsman <david.hulsman@academytrainee.com>
2020-04-03 14:43:37 +0000

    very very very poor attempt at ansible

--- script/run.sh
+++ script/run.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+#run ansible!
+#check if service is running
+#if it is destroy it
+#then rn this command
+
 docker stack deploy --compose-file ../services/docker-compose.yml rappperstack
+
