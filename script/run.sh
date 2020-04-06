a6c27deb1c8f0321962e993d4682861189d9881c
HavidDulsman <david.hulsman@academytrainee.com>
2020-04-06 15:36:29 +0000

    ansible

--- script/run.sh
+++ script/run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 #run ansible!
+sudo ansible-playbook -i inventory.cfg playbook.yml
 #check if service is running
+
 #if it is destroy it
+
 #then rn this command
 
 docker stack deploy --compose-file ../services/docker-compose.yml rappperstack
