9d7c9f913a07edf1e256f2eea1da19b60f4a8fc6
HavidDulsman <david.hulsman@academytrainee.com>
2020-04-08 09:50:44 +0000

    jenkins attempt

--- script/ansible.sh
+++ script/ansible.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # source  ~/.bashrc
 source /var/lib/jenkins/workspace/rapper-pipeline/venv/bin/activate
-ansible-playbook -i inventory.cfg playbook.yml --start-at-task="install packages"
+ansible-playbook -i inventory.cfg playbook.yml
