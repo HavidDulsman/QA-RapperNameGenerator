325f49b2f417953934811244a98380b3c90515ed
HavidDulsman <david.hulsman@academytrainee.com>
2020-04-08 14:37:16 +0000

    jenkins

--- script/docker.sh
+++ script/docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+curl https://get.docker.com | sudo bash
 source /var/lib/jenkins/workspace/"rapper-pipeline"/venv/bin/activate
 source ~/.bashrc
 sudo docker stack deploy --compose-file docker-compose.yml rapperstack 
