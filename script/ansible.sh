8eac554649bbbded0ddf0e87ccb9aca3da1af5fc
HavidDulsman <david.hulsman@academytrainee.com>
2020-04-08 09:52:23 +0000

    jenkins attempt

--- script/ansible.sh
+++ script/ansible.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-# source  ~/.bashrc
-source /var/lib/jenkins/workspace/rapper-pipeline/venv/bin/activate
-ansible-playbook -i inventory.cfg playbook.yml
+sudo apt-get update
+sudo apt-get install python -y
+# make sure ~/.local/bin exists and is on your PATH
+mkdir -p ~/.local/bin
+echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
+source ~/.bashrc
+## install ansible with pip
+pip install --user ansible
+# check that ansible has been installed
+ansible --version
+
+ansible-playbook -i inventory.cfg playbook.yml 
