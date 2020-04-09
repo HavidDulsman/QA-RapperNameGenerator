ddf44df19a1e9095acc2194bab898d11a4088677
HavidDulsman <david.hulsman@academytrainee.com>
2020-04-09 11:05:30 +0000

    test

--- script/before_installation.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudop apt update -y
-sudo apt install python3 -y
-sudo apt install python3-pip -y
-sudo apt install python3-venv -y
-sudo apt-get install libmysqlclient-dev -y
-python3 -m venv venv
